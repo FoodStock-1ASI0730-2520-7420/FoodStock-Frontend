efc36f9f9e20cb60eaa7eb5ac08a41a0f2151aa4
Melina Liz <u202216058@upc.edu.pe>
2025-10-03 00:06:06 -0500

    feature: add frontend

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import {createRouter, createWebHistory} from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
+
+// TODO: Define lazy-loaded components for routes
+const pageNotFound = () => import('./shared/presentation/views/page-not-found.vue');
+const routes = [
+    { path: '/home',            name: 'home',       component: Home,        meta: { title: 'Home' } },
+    { path: '/',                redirect: '/home' },
+    { path: '/:pathMatch(.*)*', name: 'not-found', component: pageNotFound, meta: { title: 'Page Not Found' } }
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes,
+
+});
+
+router.beforeEach((to, from, next) => {
+    console.log(`Navigating from ${from.name} to ${to.name}`);
+    // Set the page title
+    let baseTitle = 'FoodStock';
+    document.title = `${baseTitle} - ${to.meta['title']}`;
+    // TODO: Call authentication guard
+    next();
+});
+
+export default router;
